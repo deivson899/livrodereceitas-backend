85e7c9da03f0ff6f4d86752eaae6c01185779c7a
deivson <deivson899@gmail.com>
2021-07-08 14:44:11 -0300

    firts commit #1

---
+++ livrodereceitas-backend/src/main/java/br/com/livroreceitas/livrodereceitasbackend/LivrodereceitasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.livroreceitas.livrodereceitasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrodereceitasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrodereceitasBackendApplication.class, args);
+	}
+
+}
---
+++ livrodereceitas-backend/src/test/java/br/com/livroreceitas/livrodereceitasbackend/LivrodereceitasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livroreceitas.livrodereceitasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrodereceitasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
